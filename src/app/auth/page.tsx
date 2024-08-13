7a634ef2c0e7d099c9912774b7242f19ea5dc3d8
Orest Kozolup <o.kozolup.extern@instamotion.com>
2024-08-13 14:26:39 +0200

    Initial routing

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+const Auth = () => {
+  return (
+    <div style={{ height: '100vh' }}>
+      Hello World from Auth page
+      <Button variant='contained'>Click!</Button>
+    </div>
+  )
+}
+
+export default Auth;
