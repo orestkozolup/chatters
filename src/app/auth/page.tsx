9599b64b6323e083334c11c1c86b9f0564ce1480
Orest Kozolup <o.kozolup.extern@instamotion.com>
2024-08-18 16:11:42 +0200

    Correct redirect

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,34 +1,38 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function AuthPage() {
   const { data: session } = useSession();
+
+  if (session) {
+    redirect("/users");
+  }
 
   return (
     <main>
       {session ? (
         <>
           <img src={session.user?.image as string}></img>
-          <h1 className="text-3xl text-green-500 font-bold">
+          <h1>
             Welcome back, {session.user?.name}
           </h1>
-          <p className="text-2xl font-semibold">{session.user?.email}</p>
-          <button
+          <p>{session.user?.email}</p>
+          {/* <button
             onClick={() => signOut()}
             className="border border-black rounded-lg bg-red-400 px-5 py-1"
           >
             Sign Out
-          </button>
+          </button> */}
         </>
       ) : (
         <>
-          <h1 className="text-3xl text-red-500 font-bold">
-            You are not logged in
-          </h1>
-          <div className="flex space-x-5">
+          <h1>You are not logged in</h1>
+          <div>
             <button
-              onClick={() => signIn("google")}
-              className="border border-black rounded-lg px-5 py-1"
+              onClick={() =>
+                signIn("google", { callbackUrl: "http://localhost:3000/users" })
+              }
             >
               Sign in with Google
             </button>
